8a5fd09610cafad7512dcf728c1e8299b0da6b8b
Bohne9 <jonah@die-schuellers.de>
2020-11-01 19:43:19 +0100

    :sparkles: The API now supports the path /polls/latest

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const firestore = new Firestore({
 });
 
 // Firestore collection paths
-const appCollection = 'votd';
+const appCollection = 'votd/polls';
 const pollCollection = `${appCollection}/polls`;
 
 // API-URL paths
@@ -23,11 +23,35 @@ app.use('/poll/:id', (req, res) => {
      });
 });
 
-app.use('/polls/', (req, res) => {
+// This endpint will send the latest added polls to the client
+app.use('/polls/latest', (req, res) => {
+     // Set a default limit
+     var limit = 10;
 
-     return res.status(200).send({
-          message: `Fetching polls`
+     // In case the client specified a custom limit
+     // Allow max batch size of 50
+     if (req.query.limit && req.query.limit < 50) {
+          limit = req.query.limit;
+     }
+
+     const ref = firestore.collection(pollCollection);
+     
+     ref.orderBy('timestamp').limit(limit).get()
+     .then((polls) => {
+          return res.status(200).send(polls.docs);
+     })
+     .catch((err) => {
+          return res.status(404).send(value)
      });
+
+
+     // if (!polls.empty) {
+     //      return res.status(200).send(polls.docs);
+     // } else {
+          // return res.status(404).send({
+          //      message: `No polls found.`
+          // })
+     // }
 });
 
 // Make the GFS handler use the express app.
