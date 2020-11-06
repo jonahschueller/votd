4a92a5b2852b8bf052e2f0287cb53c8f52e19dc0
Bohne9 <jonah@die-schuellers.de>
2020-11-06 20:50:59 +0100

    :sparkles: The PollPreview now shows the creation date

--- votd-web/src/components/common/PollPreview.js
+++ votd-web/src/components/common/PollPreview.js
@@ -16,16 +16,23 @@ class PollPreview extends React.Component {
           return props.poll;
      }
 
+     date() {
+          let date = new Date(this.state.data.timestamp._seconds * 1000);
+
+          return date.toLocaleString();
+     }
+
      render() {
           return (
                <div class="pollPreview">
-                    <h5>{this.state.data.title}</h5>
+                    <h5 class="date">{this.date()}</h5>
+                    <h4>{this.state.data.title}</h4>
                </div>
           );
      }
 
 }
 
-export default PollPreview
+export default PollPreview;
 
 
