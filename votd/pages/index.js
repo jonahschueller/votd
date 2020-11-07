e3f3acc293cb72766c168e45ae537b27a542fda8
Bohne9 <jonah@die-schuellers.de>
2020-11-07 21:14:52 +0100

    :sparkles: Implementation of the Home view

--- votd/pages/index.js
+++ votd/pages/index.js
@@ -1,65 +1,90 @@
 import Head from 'next/head'
+import Link from "next/link";
+import React from 'react'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const apiURL = 'https://us-central1-code-it-292909.cloudfunctions.net/votd-rest-API'
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+class Home extends React.Component {
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+  static async getInitialProps(ctx) {
+    let resLatest = await fetch(`${apiURL}/polls/latest`)
+    let latestPolls = await resLatest.json()
+    
+    let resPopular = await fetch(`${apiURL}/polls/popular`)
+    let popularPolls = await resPopular.json()
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
+    return {
+      latest: latestPolls.polls.reverse(),
+      popular: popularPolls.polls
+    }
+  }
+
+  date(poll) {
+      let date = new Date(poll.data.timestamp._seconds * 1000);
+
+      return date.toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'});
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Home</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+  
+        <main className={styles.main}>
+  
+        <h4 className={styles.title}>Latest polls</h4>
+          <div className={styles.grid}>
+          {
+            this.props.latest.map(poll => {
+              return (
+                <Link href={`poll/${poll.id}`}>
+                  <div className={styles.card}>
+                    <h5>{this.date(poll)}</h5>
+                    <h3>{poll.data.title}</h3>
+                  </div>
+                </Link>
+              );
+            })
+          }
+          </div>
+  
+          <h4 className={styles.title}>Popular polls</h4>
+          <div className={styles.grid}>
+          {
+            this.props.popular.map(poll => {
+              return (
+                <Link href={`poll/${poll.id}`}>
+                  <div className={styles.card}>
+                    <h5>{this.date(poll)}</h5>
+                    <h3>{poll.data.title}</h3>
+                  </div>
+                </Link>
+              );
+            })
+          }
+          </div>
+        </main>
+  
+        <footer className={styles.footer}>
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            Powered by{' '}
+            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
           </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        </footer>
+      </div>
+    )
+  }
+  
 }
+
+export default Home
