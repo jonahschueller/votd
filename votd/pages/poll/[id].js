b3695534ebf7243ffe8f0c615fd3ccf161f83030
Bohne9 <jonah@die-schuellers.de>
2020-11-08 20:37:08 +0100

    💄 Updated UI of the Poll page.

--- votd/pages/poll/[id].js
+++ votd/pages/poll/[id].js
@@ -1,21 +1,35 @@
 import React from 'react'
+import styles from '../../styles/Poll.module.css'
 
-class Poll extends React.Component {
+const apiURL = 'https://us-central1-code-it-292909.cloudfunctions.net/votd-rest-API'
 
-     render() {
-          return (
-               <h3>{this.props.pid}</h3>
-          );
+const Poll = ({poll}) => {
+
+     const date = (p) => {
+          let d = new Date(p.data.timestamp._seconds * 1000);
+
+          return d.toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'});
      }
+
+     return (
+     <div className={styles.container}>
+          <div className={styles.card}>
+               <h3 className={styles.title}>{poll.data.title}</h3>
+
+          <h5 className={styles.description}>{date(poll)}</h5>
+
+          </div>
+     </div>);
 }
 
 export async function getServerSideProps(context) {
      const pid = context.query
 
-     console.log(pid)
+     let res = await fetch(`${apiURL}/poll/${pid.id}`)
+     let poll = await res.json()
 
      return { props: {
-               pid: pid.id
+               poll: poll
           }
      }
 }
