2524495bdc194a4bbecc8603d001cd003fb656a6
Bohne9 <jonah@die-schuellers.de>
2020-11-12 17:35:53 +0100

    💄 UI improvments of the navigation bar.

--- votd/components/navigation.js
+++ votd/components/navigation.js
@@ -10,13 +10,28 @@ const NavigationBar = () => {
                     <h3 className={styles.logo}>votd</h3>
                </Link>
                <Link href="/">
-                    <h4 className={styles.item}>Start</h4>
+                    <div className={styles.item}>
+                         <h4 className={styles.itemText}>Home</h4>
+                         <div className={styles.overlay}>
+                              <i class="fas fa-home fa-lg"></i>   
+                         </div>
+                    </div>
                </Link>
                <Link href="/search">
-                    <h4 className={styles.item}>Search</h4>
+                    <div className={styles.item}>
+                         <h4 className={styles.itemText}>Search</h4>
+                         <div className={styles.overlay}>
+                              <i class="fas fa-search fa-lg"></i>
+                         </div>
+                    </div>
                </Link>
                <Link href="/">
-                    <h4 className={styles.item}>Add a poll</h4>
+               <div className={styles.item}>
+                         <h4 className={styles.itemText}>Add a poll</h4>
+                         <div className={styles.overlay}>
+                              <i class="fas fa-plus fa-lg"></i>
+                         </div>
+                    </div>
                </Link>
           </div>
      </nav>
--- votd/pages/_app.js
+++ votd/pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import NavigationBar from '../components/navigation'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+      <script src="https://kit.fontawesome.com/4b2e2ad0a4.js" crossorigin="anonymous"></script>
+      </Head>
       <NavigationBar/>
       <Component {...pageProps}/>
     </div>
